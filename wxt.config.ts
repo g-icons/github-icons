90bf71ab00aec7e71504a8b5d00e9d4bf0ec99cd
hjoncour <hugo.joncour.k2o@gmail.com>
2026-03-12 22:35:15 -0400

    feature(icons): vscode icons

--- wxt.config.ts
+++ wxt.config.ts
@@ -8,8 +8,18 @@ export default defineConfig({
       'Replace GitHub file and folder icons with Material Design icons.',
     permissions: ['storage'],
     host_permissions: ['https://github.com/*'],
+    icons: {
+      '16': 'icon-16.png',
+      '48': 'icon-48.png',
+      '128': 'icon-128.png',
+    },
     action: {
       default_title: 'GitHub Material Icons',
+      default_icon: {
+        '16': 'icon-16.png',
+        '48': 'icon-48.png',
+        '128': 'icon-128.png',
+      },
     },
     web_accessible_resources: [
       {
