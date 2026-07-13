e433d465e7170938fb671070805402abbb64d1a6
Hugo Joncour <hugo.joncour.k2o@gmail.com>
2026-07-13 06:38:26 +0000

    fix: rename to gitIcons (#30)

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,13 +3,13 @@ import { defineConfig } from 'wxt';
 export default defineConfig({
   outDir: 'output',
   manifest: {
-    name: 'GitHub Icons',
+    name: 'GitIcons',
     description:
       'Replace GitHub file and folder icons with distinct, colorful glyphs.',
     permissions: ['storage'],
     host_permissions: ['https://github.com/*'],
     action: {
-      default_title: 'GitHub Icons',
+      default_title: 'GitIcons',
     },
     web_accessible_resources: [
       {
